1990e841eb9a4d47772e65fc5a0bae2699e24d5e
Mahmoud Agamy <agamyv@gmail.com>
2018-11-19 05:02:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.length == 0
   min_key = nil
-  min_val = Infinity
+  min_val = Float::INFINITY
   name_hash.each do |k, v|
     if v < min_val
       min_val = v 
